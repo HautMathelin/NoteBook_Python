64512e276ba7248709d4730d1ef58028b6ceb004
HautMathelin <github_dv@hautmathelin.fr>
2021-12-08 21:59:09 +0100

    Mercredi 8 decembre

---
+++ rep_boucles.py
@@ -0,0 +1,10 @@
+def for_somme():
+    texte =  "La variable s commence par prendre la valeur 0\n\n"
+    texte += "La boucle est exécutée 5 fois les lignes pour k valant successivement 2 puis 4 puis 6 puis 8 puis 10:\n"
+    texte += "     On ajoute  2 à s puis on affecte le résulat dans s (qui vaut alors 2)\n"
+    texte += "     On ajoute  4 à s puis on affecte le résulat dans s (qui vaut alors 6)\n"
+    texte += "     On ajoute  6 à s puis on affecte le résulat dans s (qui vaut alors 12)\n"
+    texte += "     On ajoute  8 à s puis on affecte le résulat dans s (qui vaut alors 20)\n"
+    texte += "     On ajoute 10 à s puis on affecte le résulat dans s (qui vaut alors 30)\n\n"
+    texte += "Puis, en sortant de la boucle, le code affiche la valeur de s"
+    return texte
